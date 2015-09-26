0887de0346d4ce263b47da6a5eb348b220baae59
Jeff Zhang <jeffzhang8716@gmail.com>
2015-09-26 23:56:41 +0800

    add client to autotest concurrency + fix zombie problem of the server

--- webserver.py
+++ webserver.py
@@ -1,9 +1,23 @@
 import socket
 import os
-import time
+import errno
+import signal
 
 SERVER_ADDRESS = (HOST, PORT) = '', 8888
-REQUEST_QUEUE_SIZE = 5
+REQUEST_QUEUE_SIZE = 1024
+
+def grim_reaper(signum, frame):
+    while True:
+        try:
+            pid, status = os.waitpid(
+                -1,
+                os.WNOHANG
+            )
+        except OSError:
+            return
+
+        if pid == 0:
+            return
 
 def handle_request(client_connection):
     request = client_connection.recv(1024)
@@ -20,7 +34,6 @@ HTTP/1.1 200 OK
 Hello, World!
 """
     client_connection.sendall(http_response)
-    time.sleep(60)
 
 def serve_forever():
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,8 +43,18 @@ def serve_forever():
     print('Serving HTTP on port {port} ...'.format(port = PORT))
     print('Parent PID (PPID): {ppid}\n'.format(ppid = os.getppid()))
 
+    signal.signal(signal.SIGCHLD, grim_reaper)
+
     while True:
-        client_connection, client_address = listen_socket.accept()
+        try:
+            client_connection, client_address = listen_socket.accept()
+        except IOError as e:
+            code, msg = e.args
+            if code == errno.EINTR:
+                continue
+            else:
+                raise
+
         pid = os.fork()
         if pid == 0:
             listen_socket.close()
